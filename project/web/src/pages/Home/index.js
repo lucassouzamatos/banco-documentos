f9886845c0c5d00d4f2d3b2d0d5be65bd4fc9227
lucassouzamatos <lucas.souza69@hotmail.com>
2019-10-06 21:19:55 -0300

    adicionando autenticacao google

--- project/web/src/pages/Home/index.js
+++ project/web/src/pages/Home/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
-
+import { GoogleLogin } from 'react-google-login';
 import { Container, ButtonContainer, Button } from './styles';
 
 export default function Home() {
   return (
     <Container>
+      <GoogleLogin
+        clientId="987341840104-akad5n2bv83oi3omgfakt76b965t46i3.apps.googleusercontent.com"
+        render={renderProps => (
+          <button onClick={renderProps.onClick} disabled={renderProps.disabled}>This is my custom Google button</button>
+        )}
+        buttonText="Login"
+        onSuccess={(response) => console.log(response)}
+        onFailure={(response) => console.log(response)}
+      />
       <ButtonContainer>
         <Button to="#" background="#292C2F">
           Sou um cliente
